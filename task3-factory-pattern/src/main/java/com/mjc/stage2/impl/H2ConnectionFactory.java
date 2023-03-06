d38ad8b4e94f73ba23ee0c6ba5438f705cf7436c
otabek <otabekeshpulatov123@gmail.com>
2023-03-06 08:41:09 +0500

    factory method

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,36 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    private static final Properties properties;
+
+    static {
+        properties = new Properties();
+        try ( Reader reader = new FileReader("src/main/resources/h2database.properties") ) {
+            properties.load(reader);
+        } catch ( IOException ex ) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public Connection createConnection() {
+        try {
+            Class.forName(properties.getProperty("jdbc_driver"));
+            return DriverManager.getConnection(properties.getProperty("db_url") , properties.getProperty("user") , properties.getProperty("password"));
+        } catch ( ClassNotFoundException | SQLException ex ) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
 
